3933b073c397c78579870573790208f11614853d
rob sullivan <rob@datachomp.com>
2015-09-14 22:29:58 -0500

    add exometer

--- mix.exs
+++ mix.exs
@@ -12,9 +12,6 @@ defmodule Nerdbeers.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [mod: {Nerdbeers, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger,
@@ -25,9 +22,6 @@ defmodule Nerdbeers.Mixfile do
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
   defp elixirc_paths(_),     do: ["lib", "web"]
 
-  # Specifies your project dependencies
-  #
-  # Type `mix help deps` for examples and options
   defp deps do
     [{:phoenix, "~> 1.0.0"},
      {:phoenix_ecto, "~> 1.1"},
@@ -35,6 +29,11 @@ defmodule Nerdbeers.Mixfile do
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"},
-     {:rollbax, "~> 0.5"}]
+     # exception
+     {:rollbax, "~> 0.5"},
+     # monitoring
+     {:exometer_core, github: "PSPDFKit-labs/exometer_core", override: true},
+     {:exometer, github: "PSPDFKit-labs/exometer"},
+     {:edown, github: "uwiger/edown", tag: "0.7", override: true}]
   end
 end
