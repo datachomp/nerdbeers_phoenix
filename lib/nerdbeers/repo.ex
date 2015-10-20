f224a4104a76a8c74a877bf7fac67594e0247e09
rob sullivan <rob@datachomp.com>
2015-10-20 00:50:55 -0500

    fix repo loggers

--- lib/nerdbeers/repo.ex
+++ lib/nerdbeers/repo.ex
@@ -3,7 +3,7 @@ defmodule Nerdbeers.Repo do
 
 
    def log(log_entry) do
-     :ok = :exometer.update ~w(nerdbeers ecto query_exec_time)a, (log_entry.query_time + log_entry.queue_time || 0) / 1_000
+     # :ok = :exometer.update ~w(nerdbeers ecto query_exec_time)a, (log_entry.query_time + log_entry.queue_time || 0) / 1_000
      :ok = :exometer.update ~w(nerdbeers ecto query_queue_time)a, (log_entry.queue_time || 0) / 1_000 # Note: You will have to add this to conf/exometer.exs if you want it
      :ok = :exometer.update ~w(nerdbeers ecto query_count)a, 1
 
